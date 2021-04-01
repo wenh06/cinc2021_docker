7db967552bc114128ae5a42885c2ec6c6925fa47
WEN Hao <wenh06@gmail.com>
2021-04-01 11:46:21 +0800

    add torch version and cuda availability check

--- torch_version.py
+++ torch_version.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     import torch
     cuda_is_available = " " if torch.cuda.is_available() else " not "
     print(f"torch version == {torch.__version__}")
-    print("cuda is{}available")
+    print(f"cuda is{cuda_is_available}available")
